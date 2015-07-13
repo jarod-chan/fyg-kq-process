ffdaa0d7d5f37a4c48c29bfcb137ca1180dface4
jhon_chen <jhon.chen@gmail.com>
2015-07-13 10:16:30 +0800

    考勤流程新版

--- fyg-kq-kaoqin/src/main/java/cn/fyg/kq/interfaces/web/module/kqbusi/kaoqin/flow/DistributeSet.java
+++ fyg-kq-kaoqin/src/main/java/cn/fyg/kq/interfaces/web/module/kqbusi/kaoqin/flow/DistributeSet.java
@@ -3,11 +3,12 @@ package cn.fyg.kq.interfaces.web.module.kqbusi.kaoqin.flow;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
-public class SkipSet implements JavaDelegate {
+public class DistributeSet implements JavaDelegate{
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		execution.setVariableLocal("isAggree", Boolean.TRUE);
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ fyg-kq-kaoqin/src/main/java/cn/fyg/kq/interfaces/web/module/kqbusi/kaoqin/flow/UnusualSet.java
@@ -0,0 +1,14 @@
+package cn.fyg.kq.interfaces.web.module.kqbusi.kaoqin.flow;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class UnusualSet  implements JavaDelegate{
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
